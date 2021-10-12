81d8df11df2fea1bfe92327a5815a638e0592088
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2021-10-12 16:49:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,119 @@
+
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+//Give specific height and width to the car image
+greencar_width = 75;
+greencar_height = 100;
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+//Set initial position for a car image.
+var greencar_x = 5;
+var greencar_y = 225;
+var background_imgTag;
+var greencar_imgTag;
+
+background_imgTag = new Image();
+background_imgTag.onload = uploadBackground;
+background_imgTag.src = background_image;
+
+greencar_imgTag = new Image();
+greencar_imgTag.onload = uploadBackground;
+greencar_imgTag.src = background_image;
+
+function add() {
+	//upload car, and background images on the canvas.
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image;
+
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadBackground;
+	greencar_imgTag.src = background_image;
+}
+
+function uploadBackground() {
+	//Define function ‘uploadBackground’
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+
+}
+
+function uploadgreencar() {
+	//Define function ‘uploadgreencar’.
+	ctx.drawImage(background_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
+
+}
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+function up()
+{
+	if(rover_y >=0)
+	{
+		rover_y = rover_y - 10;
+		console.log("When up arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		 uploadBackground();
+		 uploadrover();
+	}
+}
+function down()
+{
+	if(rover_y <=500)
+	{
+		rover_y =rover_y+ 10;
+		console.log("When down arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		 uploadrover();
+	}
+}
+function left()
+{
+	if(rover_x >= 0)
+	{
+		rover_x =rover_x - 10;
+		console.log("When left arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		 uploadrover();
+	}
+}
+function right()
+{
+	if(rover_x <= 700)
+	{
+		rover_x =rover_x + 10;
+		console.log("When right arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		uploadrover();
+   }
+}
+	
